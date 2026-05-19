e1b5aac426a56817fc31ccf622fceecaeb5bbc99
ABritex <aprelox@gmail.com>
2026-05-19 18:33:05 +0800

    Masonry Fixed

--- src/components/ui/masonry.tsx
+++ src/components/ui/masonry.tsx
@@ -2,7 +2,11 @@ import React, { useEffect, useLayoutEffect, useMemo, useRef, useState, useCallba
 import { gsap } from '#/lib/gsap';
 
 const useMedia = (queries: string[], values: number[], defaultValue: number): number => {
-    const get = () => values[queries.findIndex(q => matchMedia(q).matches)] ?? defaultValue;
+    const get = () => {
+        if (typeof window === 'undefined') return defaultValue;
+        return values[queries.findIndex(q => matchMedia(q).matches)] ?? defaultValue;
+    };
+
     const [value, setValue] = useState<number>(get);
 
     useEffect(() => {
